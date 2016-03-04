f0e99a2bf82acfb8151bc97f3821a5284c3b6b25
Hank Wang <drapho@gmail.com>
2016-03-05 02:09:41 +0900

    add three more tasks

---
+++ elementary/pangram.py
@@ -0,0 +1,14 @@
+import string
+
+
+def check_pangram(text):
+    for c in string.ascii_lowercase:
+        if c not in text.lower():
+            return False
+    return True
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
---
+++ elementary/the-end-of-other.py
@@ -0,0 +1,14 @@
+def checkio(words_set):
+    for w in words_set:
+        for ww in words_set:
+            if w != ww and w.endswith(ww):
+                return True
+    return False
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({u"hello", u"lo", u"he"}) == True, "helLO"
+    assert checkio({u"hello", u"la", u"hellow", u"cow"}) == False, "hellow la cow"
+    assert checkio({u"walk", u"duckwalk"}) == True, "duck to walk"
+    assert checkio({u"one"}) == False, "Only One"
+    assert checkio({u"helicopter", u"li", u"he"}) == False, "Only end"
